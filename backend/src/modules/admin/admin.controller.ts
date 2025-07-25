a1037e646ef9af8d94aaf5edd9da037d17d5c9ee
Dinero <dineroprojects@gmail.com>
2025-07-26 00:53:04 +0100

    backend admin account created and database is functional

--- backend/src/modules/admin/admin.controller.ts
+++ backend/src/modules/admin/admin.controller.ts
@@ -9,16 +9,21 @@ export default class AdminController {
 
   constructor() {
     this.adminService = new AdminImplementation(); // Instantiate internally
+    console.log(this.adminService)
   }
-
   public async getAdminLogedIn(req: Request, res: Response, next: NextFunction) {
+    console.log("Turbo Log  ~ AdminController ~ getAdminLogedIn ~ req.body:", req.body);
     try {
       // 1. Validate inputs (if any)
       // 2. Apply middleware before this point in Express
 
-      const { adminPassword, adminUsername } = req.body
-      const admins = await this.adminService.fetchAdminLogedIn();
-      return res.status(200).json(admins);
+      const { username, inputPassword } = req.body
+      const admins = await this.adminService.verifyAdminLogin(username, inputPassword);
+      if (admins) {
+        return res.status(200).json({ message: "admins log in successful" });
+      } else {
+        return res.status(401).json({ message: "Incorrect username or password" });
+      }
     } catch (err) {
       console.log("Turbo Log  ~ AdminController ~ getAdminLogedIn ~ err:", err);
       return res.status(500).json({ error: "Internal Server Error" });
@@ -36,24 +41,24 @@ export default class AdminController {
       return res.status(500).json({ error: "Internal Server Error" });
     }
   }
-  public async getCustomerActivation(req: Request, res: Response, next: NextFunction) {
+  public async getCustomerActivated(req: Request, res: Response, next: NextFunction) {
     try {
       // 1. Validate inputs (if any)
       // 2. Apply middleware before this point in Express
-
-      const admins = await this.adminService.fetchCustomerActivation();
+      const { userIdToBeUpdated } = req.body
+      const admins = await this.adminService.fetch_CustomerAccountActivation(userIdToBeUpdated);
       return res.status(200).json(admins);
     } catch (err) {
       console.log("Turbo Log  ~ AdminController ~ getCustomerActivation ~ err:", err);
       return res.status(500).json({ error: "Internal Server Error" });
     }
   }
-  public async getCustomerDeactivation(req: Request, res: Response, next: NextFunction) {
+  public async getCustomerDeactivated(req: Request, res: Response, next: NextFunction) {
     try {
       // 1. Validate inputs (if any)
       // 2. Apply middleware before this point in Express
-
-      const admins = await this.adminService.fetchCustomerDeactivation();
+      const { userIdToBeUpdated } = req.body
+      const admins = await this.adminService.fetch_CustomerAccountDeactivation(userIdToBeUpdated);
       return res.status(200).json(admins);
     } catch (err) {
       console.log("Turbo Log  ~ AdminController ~ getCustomerDeactivation ~ err:", err);
@@ -64,12 +69,38 @@ export default class AdminController {
     try {
       // 1. Validate inputs (if any)
       // 2. Apply middleware before this point in Express
-
-      const admins = await this.adminService.fetchIncreaseInSlot();
+      const { slotValue, schoolId } = req.body
+      const admins = await this.adminService.fetch_IncreaseCBTSlot(slotValue, schoolId);
       return res.status(200).json(admins);
     } catch (err) {
       console.log("Turbo Log  ~ AdminController ~ getIncreaseInSlot ~ err:", err);
-      console.error("Turbo Log ~ AdminController ~ getDatabaseConnectionlogic ~ err:", err);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
   }
+
+  public async getSchoolStudentForCBT(req: Request, res: Response, next: NextFunction) {
+    try {
+      // 1. Validate inputs (if any)
+      // 2. Apply middleware before this point in Express
+      const { schoolId } = req.body
+      const admins = await this.adminService.fetchSchoolManagementCBT_student(schoolId);
+      return res.status(200).json(admins);
+    } catch (err) {
+      console.log("Turbo Log  ~ AdminController ~ getSchoolStudentForCBT ~ (err:", (err));
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+  public async getSchoolManagementFullDetails(req: Request, res: Response, next: NextFunction) {
+    try {
+      // 1. Validate inputs (if any)
+      // 2. Apply middleware before this point in Express
+      const { schoolId } = req.body
+      const admins = await this.adminService.fetchSchoolManagement_alldetails(schoolId);
+      return res.status(200).json(admins);
+    } catch (err) {
+      console.log("Turbo Log  ~ AdminController ~ getSchoolManagementFullDetails ~ (err:", (err));
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+
 }
--- backend/src/modules/admin/admin.service.ts
+++ backend/src/modules/admin/admin.service.ts
@@ -3,9 +3,11 @@
  */
 import { SupabaseClient } from "@supabase/supabase-js"
 import { getSafeSupabase } from "../../config/database"
+import bcrypt from "bcrypt";
 
 export default class AdminImplementation {
   private supabase!: SupabaseClient
+  private saltRounds = 10;
 
   constructor() {
     this.init()
@@ -22,36 +24,64 @@ export default class AdminImplementation {
     }
   }
 
-  /**
-   * @description this is for testing purpose in initial setup
-   */
-  public async fetchAdminLogedIn() {
-    const { data, error } = await this.supabase.from("admins").select("*")
-    if (error) throw error
-    return data
+
+
+  public async verifyAdminLogin(username: string, inputPassword: string): Promise<boolean> {
+    console.log("Turbo Log  ~ AdminImplementation ~ verifyAdminLogin ~ verifyAdminLogin:");
+    const { data, error } = await this.supabase.rpc("get_staff_password", {
+      p_username: username
+    });
+
+    if (error || !data) return false;
+
+    const isMatch = await bcrypt.compare(inputPassword, data);
+    console.log("Turbo Log  ~ AdminImplementation ~ verifyAdminLogin ~ isMatch:", isMatch);
+    return isMatch;
   }
+
   public async fetchAllDetails() {
-    const { data, error } = await this.supabase.from("admins").select("*")
+    const { data, error } = await this.supabase.from("all_customers").select("*")
     if (error) throw error
     return data
   }
-  public async fetchCustomerActivation() {
-    const { data, error } = await this.supabase.from("admins").select("*")
+  public async fetch_CustomerAccountActivation(userIdToBeUpdated: string) {
+
+    const { data, error } = await this.supabase
+      .from('all_customers')
+      .update({ activate: true, deactivate: false })
+      .eq('user_id', userIdToBeUpdated)
+      .select()
+
     if (error) throw error
     return data
   }
-  public async fetchCustomerDeactivation() {
-    const { data, error } = await this.supabase.from("admins").select("*")
+  public async fetch_CustomerAccountDeactivation(userIdToBeUpdated: string) {
+    const { data, error } = await this.supabase
+      .from('all_customers')
+      .update({ activate: false, deactivate: true })
+      .eq('user_id', userIdToBeUpdated)
+      .select()
     if (error) throw error
     return data
   }
-  public async fetchIncreaseInSlot() {
-    const { data, error } = await this.supabase.from("admins").select("*")
+  public async fetch_IncreaseCBTSlot(slotValue: number, schoolId: string) {
+    const { data, error } = await this.supabase.from("all_customers").update({ "conputer_based_test_slot": slotValue })
+      .eq("user_id", schoolId)
+    if (error) throw error
+    return data
+  }
+  public async fetchSchoolManagementCBT_student(schoolId: string) {
+    const { data, error } = await this.supabase.from("computer_base_testing_system_db").select(`student_db(*)`)
+      .eq("school_id", schoolId)
+    if (error) throw error
+    return data
+  }
+  public async fetchSchoolManagement_alldetails(schoolId: string) {
+    const { data, error } = await this.supabase.from("school_management_system").select(`student_db(*),teacher_db(*) `).eq("school_id", schoolId)
+      .eq("school_id", schoolId)
     if (error) throw error
     return data
   }
 
-  private async validateUser() {
 
-  }
 }
