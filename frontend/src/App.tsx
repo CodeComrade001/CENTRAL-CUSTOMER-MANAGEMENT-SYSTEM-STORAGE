40267c4b1f719d65b12603569a0e404b99372334
Dinero <dineroprojects@gmail.com>
2025-07-27 01:24:02 +0100

    system base structure complete :  debuggind and connection remaining

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+import { AdminFullScreenSignIn } from "./components/ui/admin_sign_in"
+import { UserFullScreenSignUp } from "./components/ui/sign_up"
+import { UserFullScreenSignIn } from "./components/ui/sign_in"
+import { UserIntroductionPage } from "./pages/user_page"
+import { LandingPageIntroductionPage } from "./pages/landing_page"
+import UserProtectedRoute from './extermal_component/middleware/userProtectedRoute';
+import AdminProtectedRoute from "./extermal_component/middleware/adminProtectedRoute"
+import { AdminDashboard } from "./extermal_component/admin_dashboard"
+import { UserDashboard } from "./extermal_component/user_dashborad"
+import NotFoundPage from "./pages/not found_page"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes >
+        <Route path="/" element={<LandingPageIntroductionPage />} />
+        <Route path="/user" element={<UserIntroductionPage />} />
+        <Route path="/admin" element={<LandingPageIntroductionPage />} />
+        <Route path="/admin/login" element={<AdminFullScreenSignIn />} />
+        <Route path="/user/create-account" element={<UserFullScreenSignUp />} />
+        <Route path="/user/login" element={<UserFullScreenSignIn />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route
+          path="/user/dashboard"
+          element={
+            <AdminProtectedRoute isAdminAuthenticated={true} >
+              <AdminDashboard />
+            </AdminProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/dashboard"
+          element={
+            <UserProtectedRoute isUserAuthenticated={true} >
+              <UserDashboard />
+            </UserProtectedRoute>
+          }
+        />
+      </Routes>
     </>
   )
 }
